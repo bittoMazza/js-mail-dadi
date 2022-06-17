f2185eaa5b1a48dfa86c2bc89b1a331c04907f5e
bittoMazza <robertomazza99@gmail.com>
2022-06-17 15:59:35 +0200

    create one "if" out of the "for" to separate different works

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,18 +1,18 @@
 const mail = ["ciccio@gmail.com","pasticcio@gmail.com","pasticcio@hotmail.com","prova@gmail.com"];
-let message;
+let message = false;
 const userMail = prompt('Inserisci la tua mail');
 console.log("L'utente prova ad accedere con la seguente mail: "+userMail)
 for(let i=0; i < mail.length; i++){
     if(userMail === mail[i]){
-        message ="La tua mail è presente , puoi accedere !";
+        message = true;
         /* Se troviamo una mail uguale a quella inserita dall'utente,rendiamo l'indice = alla lunghezza                   
            dell'array in modo tale da non soddisfare piu la condizione del for*/ 
         i = mail.length;
     }
-    else
-    {
-        message = "La tua mail non è presente , non puoi accedere";  
-    }
 }
 
-console.log(message)
+if(message == true){
+    console.log("La mail è presente , l'utente puo accedere")
+}else{
+    console.log("La mail non è presente , l'utente non puo accedere")
+}
